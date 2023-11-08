057587b9bd694d1a7fb9a7cdc1376b562a9694e0
JuanRCoder <guillecarck@gmail.com>
2023-11-08 16:55:49 -0500

    feat: adding interface for functions

--- TypeScript/task_1/js/main.js
+++ TypeScript/task_1/js/main.js
@@ -3,8 +3,20 @@ var teacher3 = {
     firstName: 'John',
     fullTimeEmployee: false,
     lastName: 'Doe',
-    yearOfExperience: 2,
     location: 'London',
     contract: false,
 };
+var director1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
 console.log(teacher3);
+console.log(director1);
+var printTeacher = function (firstName, lastName) {
+    return "".concat(firstName[0], ". ").concat(lastName); //J. Doe
+};
+var teacher = printTeacher('Jhon', 'Doe');
+console.log(teacher);
--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -2,18 +2,42 @@ interface Teacher {
   firstName: string;
   lastName: string;
   fullTimeEmployee: boolean;
-  yearOfExperience: number;
+  yearOfExperience?: number; //optional
   location: string;
   [key: string]: any;
 };
+
+interface Directors extends Teacher{
+    numberOfReports: number;
+}
 
 const teacher3: Teacher = {
     firstName: 'John',
     fullTimeEmployee: false,
     lastName: 'Doe',
-    yearOfExperience: 2,
     location: 'London',
     contract: false,
-  };
+};
   
-  console.log(teacher3);
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+
+console.log(teacher3);
+console.log(director1);
+
+//interfaz para la funcion
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+  return `${firstName[0]}. ${lastName}` //J. Doe
+}
+
+const teacher: string = printTeacher('Jhon', 'Doe'); 
+console.log(teacher)
