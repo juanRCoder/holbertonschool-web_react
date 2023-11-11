b829877b5a0ed42ab1cb33e9efc7dfbf94cbcece
JuanRCoder <guillecarck@gmail.com>
2023-11-11 18:27:43 -0500

    feat: script and styles in body

---
+++ Webpack/task_3/modules/body/body.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import './body.css';
+
+
+$(function() {
+    $('body').append('<button>Click here to get started</button>');
+    $('body').append("<p id='count'></p>");
+    
+    function updateCounter() {
+        let count = 0;
+        return function() {
+            count += 1;
+            $('#count').text(`${count} clicks on the button`);
+        }
+    }
+
+    $('button').on('click', _.debounce( updateCounter(), 500));
+});
