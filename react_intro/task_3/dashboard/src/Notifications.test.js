e0caa2cf1e3c1f339ac22252b1425c6a57a156a9
JuanRCoder <guillecarck@gmail.com>
2023-11-18 19:28:13 -0500

    feat: test case of the component

--- react_intro/task_3/dashboard/src/Notifications.test.js
+++ react_intro/task_3/dashboard/src/Notifications.test.js
@@ -10,7 +10,7 @@ describe("Testing the <Notifications /> Component", () => {
     wrapper = shallow(<Notifications />);
   });
 
-  it("<Notifications /> is rendered without crashing", () => {
+  it("<Notifications />  is rendered without crashing", () => {
     expect(wrapper).to.not.be.an("undefined");
   });
 
@@ -18,7 +18,7 @@ describe("Testing the <Notifications /> Component", () => {
     expect(wrapper.find("li")).to.have.lengthOf(3);
   });
 
-  it("<Notifications /> render the text 'Here is the list of notifications'", () => {
+  it("<Notifications />  render the text 'Here is the list of notifications'", () => {
     expect(wrapper.contains(<p>Here is the list of notifications</p>)).to.equal(
       true
     );
