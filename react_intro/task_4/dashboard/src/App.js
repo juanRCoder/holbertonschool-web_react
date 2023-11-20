09416300edbc1fddd74758f084b742b3dca54497
JuanRCoder <guillecarck@gmail.com>
2023-11-20 00:18:31 -0500

    feat: page in githubpages

---
+++ react_intro/task_4/dashboard/src/App.js
@@ -0,0 +1,29 @@
+import HolbertonLogo from './Holberton-logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <>
+      <header className="App-header">
+        <img src={ HolbertonLogo } alt="logo-holberton" />
+        <h1>School dashboard</h1>
+      </header>
+      <main className="App-body">
+        <p>Login to access the full dashboard</p>
+        <form>
+          <label htmlFor="email">email:</label>
+          <input type='email' id='email' autoComplete='on'/>
+          <label htmlFor="password">password:</label>
+          <input type='password' id='password' autoComplete='on'/>
+          <button>OK</button>
+        </form>
+      </main>
+      <footer className="App-footer">
+      <p>Copyright { getFullYear()} - {getFooterCopy('isIndex')}</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
